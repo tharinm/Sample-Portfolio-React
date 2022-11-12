53edc8db2936148bb59e0b4bea227c2bebae67bc
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2022-11-13 00:16:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Intro } from './components/intro/Intro';
+
+function App() {
+  return (
+    <div className="App">
+      <div><Intro/></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./intro.css"
+
+export const Intro = () => {
+  return (
+    <div className="i">
+        <div className='i-left'>
+            <div className='i-left-wrapper'>
+                <h2 className='i-intro'>Hello my name is</h2>
+                <h1 className='i-name'>THARINDU</h1>
+
+                <div className='i-title'>
+                    <div className='i-title-wrapper'>
+                        <div className='i-title-item'>Web Developer</div>
+                        <div className='i-title-item'>UI/UX Designer</div>
+                        <div className='i-title-item'>Frontend Developer</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='i-right'>
+            
+        </div>
+    </div>
+  )
+}

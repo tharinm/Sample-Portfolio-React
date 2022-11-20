983cc044dd6e8acf816b1247d3132379727d4dad
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2022-11-20 08:37:41 +0530

    add data js file

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://dribbble.com/shots/16601348-Language-Translator-App?utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=Language%20Translator%20App&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=Language%20Translator%20App&utm_medium=Social_Share",
+      link: "https://dribbble.com/TH4RIN",
+    },
+    {
+      id: 2,
+      img: "https://dribbble.com/shots/16514054-Coffee-Ordering-app-Ui?utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=Coffee%20Ordering%20app%20Ui&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=Coffee%20Ordering%20app%20Ui&utm_medium=Social_Share",
+      link: "https://dribbble.com/TH4RIN",
+    },
+    {
+      id: 3,
+      img: "https://dribbble.com/shots/16472002-Game-Store-Design?utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=Game%20Store%20Design&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=Game%20Store%20Design&utm_medium=Social_Share",
+      link: "https://dribbble.com/TH4RIN",
+    },
+    {
+      id: 4,
+      img: "https://dribbble.com/shots/16472301-Event-Booking-App-Ui-Design?utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=Event%20Booking%20App%20Ui%20Design&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=Event%20Booking%20App%20Ui%20Design&utm_medium=Social_Share",
+      link: "https://dribbble.com/TH4RIN",
+    },
+    {
+      id: 5,
+      img: "https://dribbble.com/shots/16376706-Bonsai-Plant-Selling-App-Ui-Ux-Design?utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=Bonsai%20Plant%20Selling%20App%20Ui%2FUx%20Design&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=Bonsai%20Plant%20Selling%20App%20Ui%2FUx%20Design&utm_medium=Social_Share",
+      link: "https://dribbble.com/TH4RIN",
+    },
+    {
+      id: 6,
+      img: "https://dribbble.com/shots/16569381-E-commerce-shopping-App-Ui-Ux-Design?utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=E%20commerce%20shopping%20App%20Ui%20Ux%20Design&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=TH4RIN&utm_content=E%20commerce%20shopping%20App%20Ui%20Ux%20Design&utm_medium=Social_Share",
+      link: "https://dribbble.com/TH4RIN",
+    },
+  ];

e92f44c05abcff3f25f5980747bfbdd598e30f84
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2022-11-20 08:40:08 +0530

    Add main componensts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
 import { Intro } from './components/intro/Intro';
+import { About } from './components/about/About';
+import { Productlist } from './components/productlist/Productlist';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
     <div className="App">
-      <div><Intro/></div>
+      <div>  
+        <Intro/>
+        <About/>
+        <Productlist/>
+        <Contact/>
+      </div>
     </div>
   );
 }

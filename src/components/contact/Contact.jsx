6aa90bf8975b527edb512e49da7d4d5217d244fe
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2022-11-22 21:28:21 +0530

    add emiailjs api with contact form component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,25 @@ import contact from './contact.css'
 import Phone from "../../img/phone.png"
 import Email from "../../img/email.png"
 import Address from "../../img/address.png"
+import{useRef ,useState} from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+
+  const formRef =useRef()
+  const [done , setDone]=useState(false)
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+
+    emailjs.sendForm('service_97du7ob', 'template_6vx8g3u', formRef.current, '5wF2L3CDnTcDPlPod')
+    .then((result) => {
+        console.log(result.text);
+        setDone(true)
+    }, (error) => {
+        console.log(error.text);
+    });
+  }
+
   return (
     <div className="c">
     <div className="c-bg"></div>
@@ -31,6 +48,14 @@ const Contact = () => {
           <b>What’s your story?</b> Get in touch. Always available for
           freelancing if the right project comes along. me.
         </p>
+        <form ref={formRef} onSubmit={handleSubmit}>
+          <input type="text" placeholder='Name' name="user-name"/>
+          <input type="text" placeholder='Subject' name="user-subject"/>
+          <input type="text" placeholder='Email' name="user-email"/>
+          <textarea rows='5' placeholder='Message' name='message'></textarea>
+          <button>Submit</button>
+          {done && "Thank you"}
+        </form>
       </div>
     </div>
   </div>
